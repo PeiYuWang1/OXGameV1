7f0a1e956583104dcdaa437d2b3d93f4eacf95ba
PeiYuWang1 <s1132290@o365st.pu.edu.tw>
2025-03-31 11:05:55 +0800

    new

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,14 @@
+using OXGameV1;
 using System.Windows.Forms;
 
 namespace OXGame
 {
     public partial class Form1 : Form
     {
+        //
+        private OXGameEngine oxEngine = new OXGameEngine();
+        //
+        private Button[,] oxButtons = new Button[3,3]; 
         //
         private String playerMarker = "X";
 
@@ -12,6 +17,34 @@ namespace OXGame
         public Form1()
         {
             InitializeComponent();
+
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            for (int i = 0; i < 3; i++)
+            { 
+                for (int j = 0; j < 3; j++) 
+                {
+                    Button button = new Button();
+
+                    button.Font = new Font("Microsoft JhengHei UI", 32F, FontStyle.Regular, GraphicsUnit.Point);
+                    button.Size = new Size(90, 90);
+                    button.Location = new Point(30 + i*100 , 30 + j*100);
+                    button.Margin = new Padding(2);
+                    button.Name = "button" + i + j;  //
+                    button.TabIndex = i*3 + j + 1;   //
+                    button.UseVisualStyleBackColor = true;
+                    button.Click += button1_Click;
+
+                    // 註記按鈕對應的位置（x,y）
+                    button.Tag = String.Format("{0},{1}", i, j);
+
+                    this.Controls.Add(button);
+
+                    oxButtons[i,j] = button;
+                }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -33,9 +66,19 @@ namespace OXGame
         {
             // Casting sender to be a Button
             Button button = (Button)sender;
-            // 避免下第二次!
-            if (button.Text != "")
+            // 註記按鈕對應的位置（x,y）
+            //button.Tag = String.Format("{0},{1}", i, j);
+            string[] xyPos = ((String)button.Tag).Split(",");
+            int x = int.Parse(xyPos[0]);
+            int y = int.Parse(xyPos[1]);
+            //
+            if (oxEngine.IsValidMove(x, y) == false)
+            {
+                MessageBox.Show("Wrong Move!", "XXX");
                 return;
+            }
+            //
+            oxEngine.SetMarker(x , y , playerMarker[0]);
             //
             button.Text = playerMarker;
             playerMarker = playerMarker == "X" ? "O" : "X";
@@ -48,44 +91,11 @@ namespace OXGame
 
         private void checkWinner()
         {
-            // 
-            if (button1.Text != "" && button1.Text == button2.Text && button2.Text == button3.Text)
-            {
-                MessageBox.Show(button1.Text, "Winner is...");
-            }
-            else if(button4.Text != "" && button4.Text == button5.Text && button5.Text == button6.Text)
-            {
-                    MessageBox.Show(button4.Text, "Winner is...");
-            }
-            else if (button7.Text != "" && button7.Text == button8.Text && button8.Text == button9.Text)
-            {
-                MessageBox.Show(button7.Text, "Winner is...");
-            }
-            else if (button1.Text != "" && button1.Text == button4.Text && button4.Text == button7.Text)
-            {
-                MessageBox.Show(button1.Text, "Winner is...");
-            }
-            else if (button2.Text != "" && button2.Text == button5.Text && button5.Text == button8.Text)
-            {
-                MessageBox.Show(button2.Text, "Winner is...");
-            }
-            else if (button3.Text != "" && button3.Text == button6.Text && button6.Text == button9.Text)
-            {
-                MessageBox.Show(button3.Text, "Winner is...");
-            }
-            else if (button1.Text != "" && button1.Text == button5.Text && button5.Text == button9.Text)
-            {
-                MessageBox.Show(button1.Text, "Winner is...");
-            }
-            else if (button3.Text != "" && button3.Text == button5.Text && button5.Text == button7.Text)
-            {
-                MessageBox.Show(button3.Text, "Winner is...");
-            }
-            else if (counter==9)
-            {
-                // check if TIE ?
-                MessageBox.Show("平手!", "Tie...");
-            }
+            char winner = oxEngine.IsWinner();
+            if (winner == ' ')
+                return;
+            //
+            MessageBox.Show("Winner is :" + winner, "Game Over");
         }
     }
 }
---
+++ OXGameEngine.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OXGameV1
+{
+    internal class OXGameEngine
+    {
+        // 二維字元陣列來紀錄玩家的下子
+        private char[,] board;
+
+        // 建構子，初始化遊戲盤面
+        public OXGameEngine()
+        {
+            board = new char[3, 3]
+            {
+                { ' ', ' ', ' ' },
+                { ' ', ' ', ' ' },
+                { ' ', ' ', ' ' }
+            };
+        }
+
+        // (a) 設定玩家的 Marker
+        public void SetMarker(int x, int y, char player)
+        {
+            if (IsValidMove(x, y))
+            {
+                board[x, y] = player;
+            }
+        }
+
+        // (b) 重置遊戲
+        public void ResetGame()
+        {
+            board = new char[3, 3]
+            {
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' },
+            { ' ', ' ', ' ' }
+            };
+        }
+
+        // (c) 判斷是否有贏家
+        public char IsWinner()
+        {
+            // 檢查橫排、直排及對角線
+            for (int i = 0; i < 3; i++)
+            {
+                // 檢查橫排
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != ' ')
+                    return board[i, 0];
+                // 檢查直排
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != ' ')
+                    return board[0, i];
+            }
+
+            // 檢查對角線
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != ' ')
+                return board[0, 0];
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != ' ')
+                return board[0, 2];
+
+            // 若無贏家，回傳空白字元
+            return ' ';
+        }
+
+        // (d) 取得指定位置的 Marker
+        public char GetMarker(int x, int y)
+        {
+            if (x >= 0 && x < 3 && y >= 0 && y < 3)
+            {
+                return board[x, y];
+            }
+            return ' '; // 若索引不合法則回傳空白字元
+        }
+
+        // (e) 判斷指定位置是否還未下子
+        public bool IsEmpty(int x, int y)
+        {
+            if (x >= 0 && x < 3 && y >= 0 && y < 3)
+            {
+                return board[x, y] == ' ';
+            }
+            return false;
+        }
+
+        // (f) 判斷指定位置是否是有效的下子位置
+        public bool IsValidMove(int x, int y)
+        {
+            return x >= 0 && x < 3 && y >= 0 && y < 3 && IsEmpty(x, y);
+        }
+    }
+}
